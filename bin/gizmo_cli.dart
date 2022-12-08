b5cba99dce2cf1f46d2a581a3ab32145ab0640fe
Jarod Castillo <43973221+Smotto@users.noreply.github.com>
2022-12-08 14:26:42 -0800

    initial commit

---
+++ bin/gizmo_cli.dart
@@ -0,0 +1,5 @@
+import 'package:gizmo_cli/gizmo_cli.dart' as gizmo_cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${gizmo_cli.calculate()}!');
+}
---
+++ test/gizmo_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:gizmo_cli/gizmo_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
